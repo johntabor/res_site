ae9a9a9520eba6c9fe48d997ade900be51da0935
Nicholas Lusskin <nicholas.lusskin@rutgers.edu>
2018-02-10 15:45:20 -0500

    brand new site

---
+++ res18.js
@@ -0,0 +1,13 @@
+var config = {
+  apiKey: "AIzaSyBlSw_9_7ZULMKT62cQrl4zbgz6_oSQi1Q",
+  authDomain: "rutrep-27e19.firebaseapp.com",
+  databaseURL: "https://rutrep-27e19.firebaseio.com/",
+  projectId: "rutrep-27e19",
+  storageBucket: "rutrep-27e19.appspot.com",
+};
+firebase.initializeApp(config);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)};
+gtag('js', new Date());
+gtag('config', 'UA-28810488-8');
