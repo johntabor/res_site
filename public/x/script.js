748265cbe27cb87778267a034b75d679aac3b6b8
Nick <nicholas.lusskin@rutgers.edu>
2018-08-17 11:26:02 -0400

    style mods

--- public/x/script.js
+++ public/x/script.js
@@ -5,3 +5,26 @@ $('.link').hover(
     () => {
         $('#bar').animate({ width: '0px' }, 300, 'swing'); 
 });
+
+$('.link').click(
+    () => {
+        const e = $('#info');
+        e.children().hide();
+        e.animate({ width: '98vw' }, 2000, 'swing', () => {
+            e.children().fadeIn(800);
+        });
+        e.mouseleave(
+            () => {
+                e.animate({ width: '97vw' }, 200, 'swing', () => {
+                    $('*:not(.info)').on('click.info', () => {
+                        e.children().fadeOut(400);
+                        e.animate({ width: '0vw' }, 2000, 'swing');
+                        e.off();
+                        $('*:not(.info)').off('click.info');
+                    });
+                    e.mouseenter(() => e.animate({ width: '98vw' }, 200, 'swing', () => $('*:not(.info)').off('click.info')));
+                });
+                
+            });
+        
+});
